dd5d110395be9cf5b0f291577d2618cf440faa8a
Aditya <a.shekhar5247@gmail.com>
2026-03-01 16:03:38 +0530

    api:setup prisma

---
+++ apps/api/prisma.config.ts
@@ -0,0 +1,14 @@
+// @ts-check
+/// <reference types="node" />
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
